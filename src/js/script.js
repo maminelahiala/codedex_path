36b666ee52653e33aaeedd029154efa15e79ef12
Mohamed A. Lahiala <mohamedamine@lahiala.com>
2024-04-13 05:38:06 +0000

    + New Files + Created a responsive menu using CSS and JS + Added a favicon of codédex + Added the Codédex logo + Created the Quiz Page + Added onlick function on the Button of Quiz Page for an alert box + Created an alert box + Created Footer + Made the menu and footer in js script for easy integration +fixed some bugs (previous commit)

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
+/*Open a popup*/
 let popup = document.getElementById("popup");
 function donThink_open() {
     popup.classList.add("open-popup");
 }
+/*Close popup*/
 function donThink_close() {
     popup.classList.remove("open-popup");
 }
